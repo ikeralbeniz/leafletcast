67738c22e19e47df8a314b38ac154c43d6e5af67
iker <iker.perez.albeniz@gmail.com>
2015-10-27 22:58:48 +0100

    base project

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def main():
+    return render_template('index.html', posts={})
+
+@app.route('/controller/')
+def controller():
+    return render_template('controller.html', posts={})
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
